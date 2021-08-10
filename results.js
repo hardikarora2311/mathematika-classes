20e0c9fc4656bc0efb8d88110a42b725b8200c48
Hardik Arora <hardikarora826@gmail.com>
2021-08-11 04:43:53 +0530

    fix: flickity resize bug

--- results.js
+++ results.js
@@ -1,30 +1,38 @@
-var boardBtn= document.querySelector("#board")
-var mainsBtn= document.querySelector("#mains")
-var advBtn= document.querySelector("#adv")
+document.addEventListener("DOMContentLoaded", function(){
+    var boardBtn= document.querySelector("#board")
+    var mainsBtn= document.querySelector("#mains")
+    var advBtn= document.querySelector("#adv")
 
-var boardSec= document.querySelector(".slider-board")
-var mainsSec= document.querySelector(".slider-main")
-var advSec= document.querySelector(".slider-adv")
+    var boardSec= document.querySelector(".slider-board")
+    var mainsSec= document.querySelector(".slider-main")
+    var advSec= document.querySelector(".slider-adv")
+    
 
 
-boardBtn.addEventListener("click", function(){
-  boardSec.style.display="block"
-  mainsSec.style.display="none"
-  advSec.style.display="none"
+    boardBtn.addEventListener("click", function(){
+        boardSec.style.display="block"
+        mainsSec.style.display="none"
+        advSec.style.display="none"
+        var flkty = new Flickity(".slider-board > .images");
+        flkty.resize();
+    })
+    
+    
+    mainsBtn.addEventListener("click", function(){
+        boardSec.style.display="none"
+        mainsSec.style.display="block"
+        advSec.style.display="none"
+        var flkty = new Flickity(".slider-main> .images");
+        flkty.resize();
+    })
+
+    advBtn.addEventListener("click", function(){
+        boardSec.style.display="none"
+        mainsSec.style.display="none"
+        advSec.style.display="block"
+        var flkty = new Flickity( ".slider-adv > .images");
+        flkty.resize();
+    })
 
 })
 
-mainsBtn.addEventListener("click", function(){
-  boardSec.style.display="none"
-  mainsSec.style.display="block"
-  advSec.style.display="none"
-  console.log("click")
-
-})
-
-advBtn.addEventListener("click", function(){
-  boardSec.style.display="none"
-  mainsSec.style.display="none"
-  advSec.style.display="block"
-
-})
