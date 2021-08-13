9ba7534d85f7efd8701deb399182e88df06801dd
Hardik Arora <hardikarora826@gmail.com>
2021-08-13 19:13:23 +0530

    home page slider

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-var slideIndex = 0;
-carousel();
+// var slideIndex = 0;
+// carousel();
 
-function carousel() {
-    var x = document.getElementsByClassName("mySlides");
-      for (var i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-      }
-    slideIndex++;
-    if (slideIndex > x.length) {slideIndex = 1}
-    x[slideIndex-1].style.display = "block";
-    setTimeout(carousel, 4000); // Change image every 2 seconds
-}
+// function carousel() {
+//     var x = document.getElementsByClassName("mySlides");
+//       for (var i = 0; i < x.length; i++) {
+//       x[i].style.display = "none";
+//       }
+//     slideIndex++;
+//     if (slideIndex > x.length) {slideIndex = 1}
+//     x[slideIndex-1].style.display = "block";
+//     setTimeout(carousel, 4000); // Change image every 2 seconds
+// }
 
 
 
