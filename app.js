e1de70a8edd84a29d49866024d9db018f1de640f
Hardik Arora <hardikarora826@gmail.com>
2021-08-14 17:24:15 +0530

    responsive menu

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-// var slideIndex = 0;
-// carousel();
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
-// function carousel() {
-//     var x = document.getElementsByClassName("mySlides");
-//       for (var i = 0; i < x.length; i++) {
-//       x[i].style.display = "none";
-//       }
-//     slideIndex++;
-//     if (slideIndex > x.length) {slideIndex = 1}
-//     x[slideIndex-1].style.display = "block";
-//     setTimeout(carousel, 4000); // Change image every 2 seconds
-// }
+hamburger.addEventListener("click", mobileMenu);
 
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+
+const navLink = document.querySelectorAll(".link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
 
 
 
